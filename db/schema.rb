13303f762b27021e1ec9953bf3c58e6a66f60a66
James Phan <jamesvphan@gmail.com>
2017-03-13 16:42:39 -0400

    Update trip col:status to be of type string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311233757) do
+ActiveRecord::Schema.define(version: 20170313204119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20170311233757) do
   create_table "trips", force: :cascade do |t|
     t.integer  "city_id"
     t.integer  "user_id"
-    t.boolean  "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   create_table "users", force: :cascade do |t|
