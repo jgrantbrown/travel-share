b18ecf7515eb51b8e0f300eee09867e38dc5adac
James Phan <jamesvphan@gmail.com>
2017-03-15 17:06:31 -0400

    Complete edit form to use radio buttons

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,8 @@ class TripsController < ApplicationController
 
   def index
     @trips = Trip.all
-    @cities = City.all
+    #@cities = City.all
+    @user = User.find(params[:user_id])
   end
 
   def new
@@ -10,13 +11,23 @@ class TripsController < ApplicationController
   end
 
   def create
-    byebug
     @user = User.find(params[:user_id])
+    params["status"].each do |city_id, status|
+      @trip = Trip.find_by(user_id: params[:user_id], city_id: city_id.to_i)
+      if @trip
+        @trip.status = status
+        @trip.save
+      else
+        @trip = Trip.create(user_id: params[:user_id], city_id: city_id.to_i, status: status)
+      end
+    end
+    redirect_to user_trips_path(@user)
   end
 
   def edit
     @user = User.find(params[:user_id])
     @cities = City.all
+    @trips = @user.trips
   end
 
   def update
