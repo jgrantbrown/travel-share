40937ddaa395b7ee91185b7c71b314928f8574be
Lauren Grapstein <lgrapstein@gmail.com>
2017-04-16 10:12:44 -0400

    Update reviews_controller.rb
    Spacing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,7 +24,7 @@ class ReviewsController < ApplicationController
   end
 
   private
-      def review_params
-        params.require(:review).permit(:trip_id, :description, :rating)
-      end
-  end
+    def review_params
+      params.require(:review).permit(:trip_id, :description, :rating)
+    end
+ end
